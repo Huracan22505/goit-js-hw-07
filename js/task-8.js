6190e291794068617444bc0c9cbff499a0bd0e78
Huracan <huracan22505@protonmail.com>
2020-11-17 00:11:36 +0200

    8-task-fix

--- js/task-8.js
+++ js/task-8.js
@@ -3,18 +3,21 @@ const destroy = document.querySelector('button[data-action="destroy"]');
 const input = document.querySelector('#controls > input');
 const boxes = document.querySelector('#boxes');
 
+let dives = [];
+
 function createDives(amount) {
   let size = 30;
 
   for (let i = 0; i < amount; i++) {
     size += 10;
     let createDiv = document.createElement('div');
-    boxes.appendChild(createDiv);
     createDiv.style.width = `${size}px`;
     createDiv.style.height = `${size}px`;
     createDiv.style.backgroundColor =
       '#' + ((Math.random() * 0xfffff0) << 0).toString(16);
+    dives.push(createDiv);
   }
+  boxes.append(...dives);
 }
 
 render.addEventListener('click', createBoxes);
