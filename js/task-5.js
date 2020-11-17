4801c1c1bc8303e395c14c41fe2872ab4d253c04
Huracan <huracan22505@protonmail.com>
2020-11-17 10:47:01 +0200

    Правочки для Дениса))

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
+input.setAttribute('onkeyup', 'this.value = this.value.replace(/[ ]/g,"");');
+
 input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
